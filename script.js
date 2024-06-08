1c02e431f50aad9b069df1cac16d838f6b956bd7
Roman <romansking11@gmail.com>
2024-06-09 01:43:20 +0200

    Fixe main image, footer, add animation for menu btn mobile, etc

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-$(window).on("load", function () {
-  $(".loader-container").fadeout("slow");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.getElementById("menu-btn"); // Correct ID
+  const menuMobile = document.getElementById("menu-mobile");
+  const exitButton = document.getElementById("exit-btn");
+
+  if (menuButton) {
+    // Check if menuButton exists
+    menuButton.addEventListener("click", function () {
+      menuMobile.style.display = "block";
+    });
+  }
+
+  if (exitButton) {
+    // Check if menuButton exists
+    exitButton.addEventListener("click", function () {
+      menuMobile.style.display = "none";
+    });
+  }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Simulate loading time
-  setTimeout(function () {
-    // Add loaded class to body to fade out loading screen and reveal main content
-    document.body.classList.add("loaded");
-    // Allow scrolling after loading screen fades out
-    document.body.style.overflow = "auto";
-  }, 4000); // Adjust timing as needed
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const imageMain = document.getElementById("img-1"); // Correct ID
+//   const windowWidth = window.innerWidth;
+
+//   if (windowWidth < 800) {
+//     image.src = "images/creationadam/grey back model.png";
+//   } else {
+//     image.src = "images/main image back min.png";
+//   }
+// });
