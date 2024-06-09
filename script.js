55496c97e3e73bf9c9e47bcdc412cb06aa1d94b8
Roman <romansking11@gmail.com>
2024-06-09 03:59:09 +0200

    Added search bar

--- script.js
+++ script.js
@@ -3,28 +3,79 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuMobile = document.getElementById("menu-mobile");
   const exitButton = document.getElementById("exit-btn");
 
-  if (menuButton) {
-    // Check if menuButton exists
-    menuButton.addEventListener("click", function () {
-      menuMobile.style.display = "block";
-    });
-  }
+  // if (menuButton) {
+  //   // Check if menuButton exists
+  //   menuButton.addEventListener("click", function () {
+  //     menuMobile.style.display = "block";
+  //   });
+  // }
 
-  if (exitButton) {
-    // Check if menuButton exists
-    exitButton.addEventListener("click", function () {
-      menuMobile.style.display = "none";
-    });
-  }
+  menuButton.addEventListener("click", () => {
+    menuMobile.classList.add("open");
+    menuMobile.classList.remove("close");
+  });
+
+  exitButton.addEventListener("click", () => {
+    menuMobile.classList.remove("open");
+    menuMobile.classList.add("close");
+
+    setTimeout(() => {
+      menuMobile.classList.remove("close");
+    }, 1000);
+  });
+
+  // if (exitButton) {
+  //   // Check if menuButton exists
+  //   exitButton.addEventListener("click", function () {
+  //     menuMobile.style.display = "none";
+  //   });
+  // }
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const imageMain = document.getElementById("img-1"); // Correct ID
-//   const windowWidth = window.innerWidth;
+// SEARCH BAR
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBtn = document.getElementById("search-icon");
+  const mainLogo = document.getElementById("main-logo-id");
+  const searchBar = document.getElementById("search-bar-box");
+  const searchMain = document.getElementsByClassName("search-bar")[0];
+  let isSearchBarOpen = false;
 
-//   if (windowWidth < 800) {
-//     image.src = "images/creationadam/grey back model.png";
-//   } else {
-//     image.src = "images/main image back min.png";
-//   }
-// });
+  searchMain.addEventListener("click", () => {
+    console.log("click");
+  });
+
+  function toggleSearchBar() {
+    if (isSearchBarOpen) {
+      searchBar.style.width = "0";
+      searchBar.style.height = "0";
+      mainLogo.classList.remove("hidden");
+      searchMain.classList.remove("overtop");
+    } else {
+      searchBar.style.width = "100%";
+      searchBar.style.height = "100%";
+      mainLogo.classList.add("hidden");
+      searchMain.classList.add("overtop");
+    }
+    // Toggle the state of the search bar
+    isSearchBarOpen = !isSearchBarOpen;
+  }
+
+  // Event listener for the search button click
+  searchBtn.addEventListener("click", function (event) {
+    toggleSearchBar();
+    // Prevent the click event from propagating to the document
+    event.stopPropagation();
+  });
+
+  // Event listener for clicks outside the search bar
+  document.addEventListener("click", function (event) {
+    // Check if the search bar is open before handling clicks outside
+    if (
+      isSearchBarOpen &&
+      !searchBar.contains(event.target) &&
+      event.target !== searchBtn
+    ) {
+      toggleSearchBar();
+    }
+  });
+});
